6c747a693154ee81eabd3fad48a195185f50b55b
Murumuru <nguyentunglam724@gmail.com>
2023-01-21 02:59:16 +0700

    Add crawl era

--- src/main/java/crawl/Test.java
+++ src/main/java/crawl/Test.java
@@ -3,12 +3,14 @@ package crawl;
 import crawl.character.crawlCharacter;
 import crawl.event.crawlEvent;
 import crawl.site.crawlHistorySite;
+import crawl.timestamp.crawlTimeStamp;
 
 public class Test {
     public static void main(String[] args) {
 //        new crawlEvent();
 //        new crawlCharacter();
-        new crawlHistorySite();
+//        new crawlHistorySite();
+        new crawlTimeStamp();
     }
 }
 
---
+++ src/main/java/crawl/timestamp/TestLoop.java
@@ -0,0 +1,14 @@
+package crawl.timestamp;
+
+public class TestLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; ++i) {
+            if (i == 1) {
+                while (i < 3) {
+                    i++;
+                }
+            }
+            System.out.println(i);
+        }
+    }
+}
