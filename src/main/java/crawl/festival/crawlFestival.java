5c28c08445874497852c8e9d324b57ba198ee5ae
Bryan1604 <luongvuhs@gmail.com>
2023-01-20 17:14:02 +0700

    add crawl festival

---
+++ src/main/java/crawl/festival/crawlFestival.java
@@ -0,0 +1,113 @@
+package crawl.festival;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+public class crawlFestival {
+    private static Integer nbFestival;
+//    private static HashMap<Integer,String> Name ;//= new ArrayList<String>();
+//    private static HashMap<Integer,String> Location ;//= new ArrayList<String>();
+//    private static HashMap<Integer,String> Day;
+//    //private static HashMap<Integer,String> RelatedSite;
+//    //private static HashMap<Integer,String> RelatedEvent;
+//    private static HashMap<Integer,String> RelatedFigure;
+//    private static HashMap<Integer,String> FirstTime;
+//    private static HashMap<Integer,String> Note;
+
+
+    public static void main(String[] args) {
+        String url_lehoi = "https://vi.wikipedia.org/wiki/L%E1%BB%85_h%E1%BB%99i_Vi%E1%BB%87t_Nam#Danh_s%C3%A1ch_m%E1%BB%99t_s%E1%BB%91_l%E1%BB%85_h%E1%BB%99i";
+        // call scraping
+        HashMap<Integer, String> Name  = new HashMap<Integer,String>();
+        HashMap<Integer, String> Location  = new HashMap<Integer,String>();
+        HashMap<Integer, String> Day  = new HashMap<Integer,String>();
+        HashMap<Integer, String> FirstTime  = new HashMap<Integer,String>();
+        HashMap<Integer, String> RelatedFigure  = new HashMap<Integer,String>();
+        HashMap<Integer, String> Note  = new HashMap<Integer,String>();
+
+        try{
+            final Document document = Jsoup.connect(url_lehoi).get();
+            int i =1;
+            for(Element row : document.select("table.prettytable.wikitable tr")) {
+                final String day = row.select("td:nth-of-type(1)").text();
+                final String location = row.select("td:nth-of-type(2)").text();
+                final String name = row.select("td:nth-of-type(3)").text();
+                final String firstTime = row.select("td:nth-of-type(4)").text();
+                final String relatedFigure = row.select("td:nth-of-type(5)").text();
+                final String note = row.select("td:nth-of-type(6)").text();
+
+                addHashmap(i, Name, name);
+                addHashmap(i, Location, location);
+                addHashmap(i, Day, day);
+                addHashmap(i, FirstTime, firstTime);
+                addHashmap(i, RelatedFigure, relatedFigure);
+                addHashmap(i, Note, note);
+                nbFestival = i;
+                i++;
+                System.out.println("id: "+ nbFestival);
+                System.out.println(Name.get(nbFestival));
+                System.out.println(Location.get(nbFestival));
+                System.out.println(Day.get(nbFestival));
+                System.out.println(RelatedFigure.get(nbFestival));
+                System.out.println(FirstTime.get(nbFestival));
+                System.out.println(Note.get(nbFestival));
+                System.out.println("------");
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    //scaping
+//    public static void scraping(String url){
+//        try{
+//            final Document document = Jsoup.connect(url).get();
+//           // System.out.println(document.outerHtml());
+//            int i =1;
+//            for(Element row : document.select("table.prettytable.wikitable tr")){
+//                final String day= row.select("td:nth-of-type(1)").text();
+//                final String location =  row.select("td:nth-of-type(2)").text();
+//                final String name =  row.select("td:nth-of-type(3)").text();
+//                final String firstTime =  row.select("td:nth-of-type(4)").text();
+//                final String relatedFigure =  row.select("td:nth-of-type(5)").text();
+//                final String note =  row.select("td:nth-of-type(6)").text();
+//
+//                addHashmap(i,Name,name);
+//                addHashmap(i,Location,location);
+//                addHashmap(i,Day,day);
+//                addHashmap(i,FirstTime,firstTime);
+//                addHashmap(i,RelatedFigure,relatedFigure);
+//                addHashmap(i,Note,note);
+//                nbFestival = i;
+//                i++;
+//            }
+//
+//        }catch (Exception ex){
+//            ex.printStackTrace();
+//        }
+//    }
+
+    public static void addHashmap(int i,HashMap<Integer,String> list,String text){
+        if(text.equals("")){
+            list.put(i,"None");
+        }else{
+            list.put(i,text);
+        }
+    }
+
+    public static void show(HashMap<Integer, String> hashMap) {
+        Set<Integer> keySet = hashMap.keySet();
+        for (Integer key : keySet) {
+            System.out.println(key + " - " + hashMap.get(key));
+        }
+    }
+}
+
+
