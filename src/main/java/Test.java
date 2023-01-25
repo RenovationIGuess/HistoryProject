5d2c2ff074396e410f4dc9675a6158c57f42187a
Murumuru <nguyentunglam724@gmail.com>
2023-01-25 17:20:17 +0700

    testing json second time

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,9 +3,10 @@ import history.event.Events;
 import history.festival.Festivals;
 import history.historicalfigure.HistoricalFigures;
 import history.historicsite.HistoricSites;
+import history.relation.Relation;
 
 public class Test {
     public static void main(String[] args) {
-
+        new Relation();
     }
 }
