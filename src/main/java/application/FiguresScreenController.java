3e51d932df9ce3452775bdb1539f50e23ae0b036
locpt-0253 <pham.tien.loc@sun-asterisk.com>
2023-01-28 23:36:34 +0700

    add search by id and name function

--- src/main/java/application/FiguresScreenController.java
+++ src/main/java/application/FiguresScreenController.java
@@ -34,6 +34,71 @@ public class FiguresScreenController implements Initializable {
     void getData(){
         List<HistoricalEntity> list = HistoricalFigures.collection.getData();
         listOfFigures = FXCollections.observableList(list);
+        searchBarController.setSearchBoxListener(
+                new SearchBoxListener() {
+                    @Override
+                    public void onSearchNameHandler(String text) {
+                        listOfFigures = FXCollections.observableArrayList(
+                                HistoricalFigures.collection.searchByName(text)
+                        );
+                        updateContent();
+                    }
+
+                    @Override
+                    public void onSearchIdHandler(String id) {
+                        int searchId;
+                        try {
+                            searchId = Integer.parseInt(id);
+                            listOfFigures = FXCollections.observableArrayList(
+                                    HistoricalFigures.collection.get(searchId)
+                            );
+                        } catch (Exception e) {
+                            System.out.println("Cannot find the entity with the id " + id);
+                        }
+                        updateContent();
+                    }
+
+                    @Override
+                    public void onBlankHandler() {
+                        listOfFigures = HistoricalFigures.collection.getData();
+                        updateContent();
+                    }
+                }
+        );
+    }
+
+    void updateContent(){
+        grid.getChildren().removeAll(grid.getChildren());
+        int column = 0;
+        int row = 0;
+        try {
+            for (HistoricalEntity entity : listOfFigures){
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(App.convertToURL("/application/fxml/Item.fxml"));
+                HBox hbox = loader.load();
+
+                ItemController itemController = loader.getController();
+                itemController.setEntity(entity);
+
+                if (column == 3) {
+                    column = 0;
+                    row++;
+                }
+
+                grid.add(hbox, column++, row);
+                grid.setMinWidth(Region.USE_COMPUTED_SIZE);
+                grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                grid.setMaxWidth(Region.USE_PREF_SIZE);
+
+                grid.setMinHeight(Region.USE_COMPUTED_SIZE);
+                grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+                grid.setMaxHeight(Region.USE_PREF_SIZE);
+
+                GridPane.setMargin(hbox, new Insets(5));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
---
+++ src/main/java/application/SearchBoxListener.java
@@ -0,0 +1,7 @@
+package application;
+
+public interface SearchBoxListener {
+    public void onSearchNameHandler(String text);
+    public void onSearchIdHandler(String text);
+    public void onBlankHandler();
+}
