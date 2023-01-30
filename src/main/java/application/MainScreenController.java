d0cf10a1d23ec006a337d6bcbe5ecaacd772d5c1
locpt-0253 <pham.tien.loc@sun-asterisk.com>
2023-01-30 23:31:05 +0700

    fix to table view and layout

--- src/main/java/application/MainScreenController.java
+++ src/main/java/application/MainScreenController.java
@@ -1,10 +1,8 @@
 package application;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,11 +10,6 @@ public class MainScreenController implements Initializable {
 
     @FXML
     private SidebarController sideBarController;
-
-    @FXML
-    void switchToInstructionScreen(ActionEvent event) throws IOException {
-        sideBarController.switchByGetFxml("/application/fxml/InstructionScreen.fxml", event);
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
--- src/main/java/application/SidebarController.java
+++ src/main/java/application/SidebarController.java
@@ -3,17 +3,14 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class SidebarController implements Initializable {
+public class SidebarController {
 
     private Parent root;
     private Stage stage;
@@ -63,13 +60,4 @@ public class SidebarController implements Initializable {
         switchByGetFxml("/application/fxml/TrangTimKiemLeHoi.fxml", event);
     }
 
-    @FXML
-    void switchToInstruction(ActionEvent event) throws IOException {
-        switchByGetFxml("/application/fxml/InstructionScreen.fxml", event);
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
 }
