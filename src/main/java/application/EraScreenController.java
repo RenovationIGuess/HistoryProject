29343875cf336ccceaf50562169e7cfc0ef98ad4
Murumuru <nguyentunglam724@gmail.com>
2023-01-31 01:04:19 +0700

    add table to screens

--- src/main/java/application/EraScreenController.java
+++ src/main/java/application/EraScreenController.java
@@ -5,8 +5,12 @@ import history.era.Era;
 import history.era.Eras;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.List;
@@ -15,18 +19,38 @@ import java.util.ResourceBundle;
 public class EraScreenController implements Initializable {
     private FiguresScreenController figuresScreenController;
 
-    private TableView<HistoricalEntity> eraTable;
+    @FXML
+    private TableView eraTable;
+
+    @FXML
+    private TableColumn colEraId;
+
+    @FXML
+    private TableColumn colEraName;
+
+    @FXML
+    private TableColumn colEraDate;
+
+    @FXML
+    private TableColumn colEraTimeStamp;
 
     private ObservableList<HistoricalEntity> listOfEras;
 
-    void getData() {
-        List<HistoricalEntity> eras = Eras.collection.getData();
-        listOfEras = FXCollections.observableList(eras);
-    }
-
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Eras.loadJSON();
-        getData();
-        eraTable.setItems(this.listOfEras);
+
+        colEraId.setCellValueFactory(
+                new PropertyValueFactory<Era, Integer>("id")
+        );
+        colEraName.setCellValueFactory(
+                new PropertyValueFactory<Era, String>("name")
+        );
+        colEraDate.setCellValueFactory(
+                new PropertyValueFactory<Era, String>("time")
+        );
+        colEraTimeStamp.setCellValueFactory(
+                new PropertyValueFactory<Era, String>("belongsToTimestamp")
+        );
+        eraTable.setItems(Eras.collection.getData());
     }
 }
